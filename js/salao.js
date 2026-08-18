95effcb0f94eabecf599ad8a971dcaea6335f933
nickiioch <nicole.ochay1@gmail.com>
2026-08-18 15:21:01 -0300

    Add files via upload

--- js/salao.js
+++ js/salao.js
@@ -1,35 +1,27 @@
 function cadastrarAgendamento(){
 
     let nome = document.getElementById("nome").value;
-
     let profissional = document.getElementById("profissional").value;
-
-    let sexo = document.querySelector('input[nome="sexo"]:checked').value;
-
+    let sexo = document.querySelector('input[name="sexo"]:checked').value;
     let data = document.getElementById("data").value;
-
     let horario = document.getElementById("horario").value;
 
-    let servicos = document.querySelectorAll (".serv:ckecked");
-
-    let listaServicos = []; 
+    let servicos = document.querySelectorAll(".serv:checked");
+    let listaServicos = [];
 
     servicos.forEach(function(servicos){
-        
+    
         listaServicos.push(servicos.value);
+    });
 
-    });   
-
-        localStorage.setItem("nomeCliente",nome);
-        localStorage.setItem("profissional",profissional);
-        localStorage.setItem("sexoCliente",sexo);
-        localStorage.setItem("listaServicos",listaServicos.join(","));
-        localStorage.setItem("dataAtendimento",data);
-        localStorage.setItem("horarioAtendimento",horario);
-
-        window.location.href="comprovante.html";
-
-    }
+    localStorage.setItem("nomeCliente", nome);
+    localStorage.setItem("profissional", profissional);
+    localStorage.setItem("sexoCliente", sexo);
+    localStorage.setItem("listaServicos", listaServicos.join(","));
+    localStorage.setItem("data", data);
+    localStorage.setItem("horario", horario);
 
 
+    window.location.href = "comprovante.html";
 
+}
